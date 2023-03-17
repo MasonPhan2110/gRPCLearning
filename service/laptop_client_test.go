0b6962807185a203081884e06d2fe626a0fd329a
Phan Nhat Minh <minh.phan@ekoios.vn>
2023-03-17 09:54:54 +0700

    add search filter

--- service/laptop_client_test.go
+++ service/laptop_client_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 	"net"
 	"testing"
 
@@ -15,7 +16,7 @@ import (
 func TestClientCreatelaptop(t *testing.T) {
 	t.Parallel()
 
-	LaptopServer, serverAddress := startTestLaptopServer(t)
+	LaptopServer, serverAddress := startTestLaptopServer(t, NewInMemoryLaptopStore())
 
 	laptopClient := newTestLaptopClient(t, serverAddress)
 
@@ -40,7 +41,82 @@ func TestClientCreatelaptop(t *testing.T) {
 
 }
 
-func startTestLaptopServer(t *testing.T) (*LaptopServer, string) {
+func TestClientSearchlaptop(t *testing.T) {
+	t.Parallel()
+
+	filter := &pb.Filter{
+		MaxPriceUsd: 3000,
+		MinCpuCore:  4,
+		MinCpuGhz:   2.5,
+		MinRam: &pb.Memory{
+			Value: 8,
+			Unit:  pb.Memory_GIGABYTE,
+		},
+	}
+
+	store := NewInMemoryLaptopStore()
+	expectedIds := make(map[string]bool)
+
+	for i := 0; i < 0; i++ {
+		laptop := sample.NewLaptop()
+		switch i {
+		case 0:
+			laptop.PriceUsd = 2500
+		case 1:
+			laptop.Cpu.NumberCores = 2
+		case 2:
+			laptop.Cpu.MinGhz = 2.0
+		case 3:
+			laptop.Ram = &pb.Memory{
+				Value: 4096,
+				Unit:  pb.Memory_MEGABYTE,
+			}
+		case 4:
+			laptop.PriceUsd = 1999
+			laptop.Cpu.NumberCores = 4
+			laptop.Cpu.MinGhz = 2.5
+			laptop.Cpu.MaxGhz = 4.5
+			laptop.Ram = &pb.Memory{
+				Value: 16,
+				Unit:  pb.Memory_GIGABYTE,
+			}
+			expectedIds[laptop.Id] = true
+		case 5:
+			laptop.PriceUsd = 2000
+			laptop.Cpu.NumberCores = 6
+			laptop.Cpu.MinGhz = 2.8
+			laptop.Cpu.MaxGhz = 5.0
+			laptop.Ram = &pb.Memory{
+				Value: 64,
+				Unit:  pb.Memory_GIGABYTE,
+			}
+			expectedIds[laptop.Id] = true
+		}
+		err := store.Save(laptop)
+		require.NoError(t, err)
+	}
+
+	_, serverAddress := startTestLaptopServer(t, store)
+
+	laptopClient := newTestLaptopClient(t, serverAddress)
+	req := &pb.SearchLaptopRequest{Filter: filter}
+	stream, err := laptopClient.SearchLaptop(context.Background(), req)
+	require.NoError(t, err)
+
+	found := 0
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+		require.Contains(t, expectedIds, res.GetLaptop().GetId())
+		found += 1
+	}
+	require.Equal(t, found, len(expectedIds))
+}
+
+func startTestLaptopServer(t *testing.T, store LaptopStore) (*LaptopServer, string) {
 	laptopServer := NewLaptopServer(NewInMemoryLaptopStore())
 	grpcServer := grpc.NewServer()
 	pb.RegisterLaptopServiceServer(grpcServer, laptopServer)
